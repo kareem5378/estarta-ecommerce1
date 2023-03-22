41706a5fefe4e4fe6cc55bdc0554c89d9c89b92a
kareem5378 <108336308+kareem5378@users.noreply.github.com>
2023-03-22 02:29:47 -0900

    Update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 
--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@
 import { combineReducers, applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import storage from "redux-persist/lib/storage";
 
 //Reducer imports
 import Products from "../reducers/Products/reducer";
@@ -12,24 +12,20 @@ import HandleCart from "../reducers/Cart/reducer";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["cartReducer"], // Add any reducer you want to persist in the local storage
 };
-
-const store = createStore(
-  persistedReducer,
-  allReducers,
-  {},
-  applyMiddleware(thunk)
-);
-
-const persistor = persistStore(store);
-
-const persistedReducer = persistReducer(persistConfig, AllReducers);
 
 const allReducers = combineReducers({
   Products,
   Auth,
   HandleCart,
 });
+const persistedReducer = persistReducer(persistConfig, allReducers);
+const store = createStore(
+  persistedReducer,
+  {},
+  applyMiddleware(thunk)
+);
+
+const persistor = persistStore(store);
 
 export { store, persistor };
